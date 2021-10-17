2d0f6098f097d1c38f1ea0ad6be6d176c3051dbc
ialtayeb21 <92675020+ialtayeb21@users.noreply.github.com>
2021-10-17 14:28:28 +0200

    Initialize rebrand-ui

---
+++ erpnext/loan_management/doctype/loan_security_type/loan_security_type_dashboard.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return {
+		'fieldname': 'loan_security_type',
+		'transactions': [
+			{
+				'items': ['Loan Security Pledge', 'Loan Security Unpledge']
+			}
+		]
+	}
---
+++ erpnext/loan_management/doctype/process_loan_security_shortfall/process_loan_security_shortfall.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2019, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Process Loan Security Shortfall', {
+	onload: function(frm) {
+		if (frm.is_new()) {
+			frm.set_value('update_time', frappe.datetime.now_datetime());
+		}
+	}
+});
